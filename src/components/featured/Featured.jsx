a54d31546271ae13c70fa46571d685e1ad8bda8e
usmansy <uzman.xtm@gmail.com>
2022-05-11 15:31:47 +0700

    tambahan kedua

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,31 @@
+import "./featured.css"
+
+const Featured = () => {
+    return (
+        <div className="featured">
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Pangandaran</h1>
+                    <h2>di Pangandaran</h2>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Batu Karas</h1>
+                    <h2>di Batu Karas</h2>
+                </div>
+            </div>
+            <div className="featuredItem">
+                <img src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o=" alt="" className="featuredImg" />
+                <div className="featuredTitles">
+                    <h1>Citumang</h1>
+                    <h2>di Citumang</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Featured
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,8 @@
+import Featured from "../../components/featured/Featured"
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties"
 import Header from "../../components/header/Header"
 import Navbar from "../../components/navbar/Navbar"
+import PropertyList from "../../components/propertyList/PropertyList"
 import "./home.css"
 
 const Home = () => {
@@ -7,6 +10,13 @@ const Home = () => {
         <div>
             <Navbar />
             <Header />
+            <div className="homeContainer">
+                <Featured />
+                <h1 className="homeTitle">Browse by property type</h1>
+                <PropertyList />
+                <h1 className="homeTitle">Homes guests love</h1>
+                <FeaturedProperties />
+            </div>
         </div>
     )
 }
